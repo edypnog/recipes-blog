50845f301dd4134ca6901d839bb0b9825100c8c0
Edy Nogueira <edlainenogueira9@gmail.com>
2022-10-22 19:30:32 -0300

    models

--- recipesRoot/settings.py
+++ recipesRoot/settings.py
@@ -121,13 +121,16 @@ USE_TZ = True
 STATIC_URL = 'recipes/static/'
 STATIC_URL = 'static/'
 
+STATIC_ROOT = 'static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
 ]
 
-STATIC_ROOT = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
